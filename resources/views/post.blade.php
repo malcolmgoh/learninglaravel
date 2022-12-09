f02a9db3f481dd8620134f5ec3c2eb077f879056
Warrence Lim <warrence@gmail.com>
2022-12-10 00:06:42 +0800

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+    <?= $post; ?>
+
+    </article>
+
+<a href="/">Go Back</a>
+
+</body>
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <?php foreach ($posts as $post) :?>
+    <article>
+        
+
+    </article>
+<?php endforeach; ?>
+</body>
+
+
